f8d91c18eea2daea53797e88afb58c3bc1f08a9c
Jan Mrkos <mrkosja1@fel.cvut.cz>
2022-06-13 14:53:40 +0200

    Added some tests with "infinite" binary tree

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,61 +3,69 @@ using JSON
 using Test
 using NBInclude
 using Base64
+using AbstractTrees
 
-children = [[2,3], [], [4], []]
-text = ["one\n(second line)", "2", "III", "four"]
-style = ["", "fill:red", "r:14", "opacity:0.7"]
-link_style = ["", "stroke:blue", "", "stroke-width:10px"]
-tooltip = ["pops", "up", "on", "hover"]
-t1 = D3Tree(children,
-           text=text,
-           style=style,
-           tooltip=tooltip,
-           link_style=link_style)
 
-@show json(t1)
-
-stringmime(MIME("text/html"), t1)
-
-t2 = D3Tree(children,
+@testset "simple tree" begin
+    children = [[2,3], [], [4], []]
+    text = ["one\n(second line)", "2", "III", "four"]
+    style = ["", "fill:red", "r:14", "opacity:0.7"]
+    link_style = ["", "stroke:blue", "", "stroke-width:10px"]
+    tooltip = ["pops", "up", "on", "hover"]
+    t1 = D3Tree(children,
             text=text,
             style=style,
             tooltip=tooltip,
-            link_style=link_style,
-            init_expand=1000,
-            init_duration=5000,
-            svg_height=300
-           )
+            link_style=link_style)
 
-# inchrome(t1)
-# inbrowser(t1, "firefox")
-# inchrome(t2)
-# inbrowser(t2, "firefox")
+    @show json(t1)
 
-@show D3Trees.children(D3TreeNode(t1, 1))
-show(stdout, MIME("text/plain"), t1)
+    stringmime(MIME("text/html"), t1)
 
-@nbinclude("../examples/hello.ipynb")
+    t2 = D3Tree(children,
+                text=text,
+                style=style,
+                tooltip=tooltip,
+                link_style=link_style,
+                init_expand=1000,
+                init_duration=5000,
+                svg_height=300
+            )
 
-n = 1_000_000
-println("generating $n children")
-@time begin
-    children = [[i+1] for i in 1:n-1]
-    push!(children, [])
+    # inchrome(t1)
+    # inbrowser(t1, "firefox")
+    # inchrome(t2)
+    # inbrowser(t2, "firefox")
+
+    @show D3Trees.children(D3TreeNode(t1, 1))
+    show(stdout, MIME("text/plain"), t1)
+
+    @nbinclude("../examples/hello.ipynb")
+
+    n = 1_000_000
+    println("generating $n children")
+    @time begin
+        children = [[i+1] for i in 1:n-1]
+        push!(children, [])
+    end
+
+    println("creating tree object")
+    @time t = D3Tree(children)
+
+    println("html string")
+    @time stringmime(MIME("text/html"), t)
+
+    println("AbstractTrees constructor")
+    @time t3 = D3Tree(t1)
+    @time t4 = D3Tree(t1, detect_repeat=false)
+    @test t3.text == t1.text
+    @test t3.tooltip == t1.tooltip
+    @test t3.style == t1.style
+    @test t3.link_style == t1.link_style
+
+    inbrowser(D3Tree([[2],[]]), `ls`)
 end
 
-println("creating tree object")
-@time t = D3Tree(children)
-
-println("html string")
-@time stringmime(MIME("text/html"), t)
-
-println("AbstractTrees constructor")
-@time t3 = D3Tree(t1)
-@time t4 = D3Tree(t1, detect_repeat=false)
-@test t3.text == t1.text
-@test t3.tooltip == t1.tooltip
-@test t3.style == t1.style
-@test t3.link_style == t1.link_style
-
-inbrowser(D3Tree([[2],[]]), `ls`)
+@testset "infinite tree" begin
+    include("testInfiniteTree.jl")
+end
---
+++ test/testInfiniteTree.jl
@@ -0,0 +1,55 @@
+import Pkg;
+Pkg.activate("test");
+using Debugger
+using D3Trees
+using JSON
+using Test
+using NBInclude
+using Base64
+using AbstractTrees
+
+module TestTrees
+using AbstractTrees
+    include("binaryAbstractTrees.jl")
+end
+
+
+@testset "expand node" begin
+    ldroot = TestTrees.LimitedDepthTree(1)
+    t1 = D3Tree(ldroot, max_expand_depth=1)
+    D3Trees.expand_node!(t1, 2, 1)
+    D3Trees.expand_node!(t1, 3, 1)
+    D3Trees.expand_node!(t1, 4, 1)
+    D3Trees.expand_node!(t1, 5, 1)
+    D3Trees.expand_node!(t1, 6, 1)
+    D3Trees.expand_node!(t1, 7, 1)
+
+    t2 = D3Tree(ldroot, max_expand_depth=1)
+    D3Trees.expand_node!(t2, 2, 2)
+    D3Trees.expand_node!(t2, 3, 2)
+
+    t3 = D3Tree(ldroot, max_expand_depth=3)
+
+    # The trees are grown differently, so indeces can be ordered differently
+    @test length(t1.children) == length(t2.children) == length(t3.children)
+    @test sort(t1.text) == sort(t2.text) == sort(t3.text)
+    @test sort(t1.tooltip) == sort(t2.tooltip) == sort(t2.tooltip)
+    @test sort(t1.style) == sort(t2.style) == sort(t2.style)
+    @test sort(t1.link_style) == sort(t2.link_style) == sort(t2.link_style)
+    t1_unexplored_children = sort(collect(values(t1.unexpanded_children)); lt=(x, y) -> x.id < y.id)
+    t2_unexplored_children = sort(collect(values(t2.unexpanded_children)); lt=(x, y) -> x.id < y.id)
+    t3_unexplored_children = sort(collect(values(t3.unexpanded_children)); lt=(x, y) -> x.id < y.id)
+    @test t1_unexplored_children == t2_unexplored_children == t3_unexplored_children
+
+    # But they should represent the same tree
+    io = IOBuffer()
+    show(IOContext(io, :limit => true, :displaysize => (10, 10)), "text/plain", t1)
+    s1 = String(take!(io))
+
+    io = IOBuffer()
+    show(IOContext(io, :limit => true, :displaysize => (10, 10)), "text/plain", t2)
+    s2 = String(take!(io))
+
+    @test s1 == s2
+end
+
