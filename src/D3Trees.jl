8a42368350d86a44a0b9938ebd9208a8438e4b00
Jan Mrkos <mrkosja1@fel.cvut.cz>
2022-07-15 16:34:12 +0200

    Updated kwargs in notebook.

--- src/D3Trees.jl
+++ src/D3Trees.jl
@@ -57,8 +57,8 @@ potentially holding up a lot of memory. To reset the server and remove reference
 
 - `detect_repeat`: if true, uses a dictionary to detect whether a node has appeared previously
 - `lazy_expand_after_depth::Integer`: default 20. Sets tree depth to at which `AbstractTrees.children(node)` will not be called anymore. Instead, nodes at this depth are cached and `children(node)` is called only when node is clicked in the D3 interactive visualization. Root has depth 0, so setting `lazy_expand_after_depth=1` expands only the root.
-- `port::Integer` - (default: 16370) specify server port for D3Trees server that will serve subtrees for visualization. Shutdown server by `shutdown_server(port)`.
 - `lazy_subtree_depth::Integer` - (default: 2) sets depth of subtrees fetched from D3Trees server
+- `port::Integer` - (default: 16370) specify server port for D3Trees server that will serve subtrees for visualization. Shutdown server by `shutdown_server(port)`.
 - `dry_run_lazy_vizualization::Function` - (default: t -> D3Trees.dry_run_server(port, t)) function that is ran once before visualization is started to speed up first fetch in the visualization. Provide custom function if your tree's children method takes a long time on first run.
 - Also supports, the non-vector arguments of the vector-of-vectors `D3Tree` constructor, i.e. `title`, `init_expand`, `init_duration`, `svg_height`.
 """
